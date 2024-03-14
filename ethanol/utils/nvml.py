a58c2c90bbe745fb404c734418a75697efb7d71f
tretrauit <tretrauit@gmail.com>
2024-03-14 18:12:14 +0700

    fix: wine-nvml building should now work again

--- ethanol/utils/nvml.py
+++ ethanol/utils/nvml.py
@@ -1,23 +1,24 @@
 """wine-nvml builder and copy to Wine output directory"""
 import subprocess
-from ethanol.utils.logger import debug, info, warn, error
+from ethanol.utils.logger import logger as base_logger
 from pathlib import Path
 
 BASE_PATH = Path("./src/wine-nvml")
+logger = base_logger.getChild("nvml")
 
 
 def update_source():
     """Update wine-nvml source"""
     if BASE_PATH.exists():
-        info("Updating wine-nvml repo...")
+        logger.info("Updating wine-nvml repo...")
         retcode = subprocess.call(["git", "pull", "--rebase"], cwd=BASE_PATH)
         if retcode != 0:
-            warn("Updating repo failed, resetting...")
+            logger.warn("Updating repo failed, resetting...")
             subprocess.call(["git", "reset", "--hard"], cwd=BASE_PATH)
-            info("Updating wine-nvml repo again...")
+            logger.info("Updating wine-nvml repo again...")
             subprocess.call(["git", "pull"], cwd=BASE_PATH)
     else:
-        info("Cloning wine-nvml repo...")
+        logger.info("Cloning wine-nvml repo...")
         subprocess.call(
             [
                 "git",
@@ -26,16 +27,18 @@ def update_source():
                 str(BASE_PATH),
             ]
         )
-    info("Updated sources.")
+    logger.info("Updated sources.")
 
 
 def build():
     """Build wine-nvml"""
-    info("Building wine-nvml...")
+    logger.info("Resetting repo state (to fix building issue)...")
+    subprocess.call(["git", "reset", "--hard"], cwd=BASE_PATH)
+    logger.info("Building wine-nvml...")
     retcode = subprocess.call(["bash", "./build.sh"], cwd=BASE_PATH)
     if retcode == 0:
-        info("Built wine-nvml.")
+        logger.info("Built wine-nvml.")
         return True
     else:
-        error("Failed to build wine-nvml.")
+        logger.error("Failed to build wine-nvml.")
         return False
